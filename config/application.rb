994753259bb745ca31c8ecd6d770914566f10ee0
Ben Maurer <ben.maurer14@gmail.com>
2023-12-16 21:29:08 +0100

    Revert "Added Devise and its functions, added User model and database"
    This reverts commit 5a6f0ab92d943a6a1692dac7e4cca7e86791dc5c.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -16,11 +16,7 @@ module IftWebsite
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = 'Central Time (US & Canada)'
+    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    # Language configs
-    I18n.available_locales = %i[en de]
-    # config.i18n.default_locale = :de
   end
 end
