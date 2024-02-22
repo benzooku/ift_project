787ee552bb32cf3b0c7e1b0514e9e878f53cf060
Ben Maurer <ben.maurer14@gmail.com>
2024-02-22 19:02:08 +0100

    database for dev and testing switched to sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_12_22_183428) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
