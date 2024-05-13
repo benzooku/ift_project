772cdae4bccbb4119030c2a7e7e5381ff6fa69d3
byMikiexe <miki.gibu@gmai.com>
2024-05-13 08:32:34 +0000

    added Real Header, href not added, colors look bad

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  darkMode:'class',
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
@@ -9,15 +10,36 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
+      colors: {
+        primary: {"50":"#f5f3ff","100":"#ede9fe","200":"#ddd6fe","300":"#c4b5fd","400":"#a78bfa","500":"#8b5cf6","600":"#7c3aed","700":"#6d28d9","800":"#5b21b6","900":"#4c1d95","950":"#2e1065"}
+      }
+    },
+      fontFamily: { 
+        'body': [
+        'Inter', 
+        'ui-sans-serif', 
+        'system-ui', 
+        '-apple-system', 
+        'system-ui', 
+        'Segoe UI', 
+        'Roboto', 
+        'Helvetica Neue', 
+        'Arial', 
+        'Noto Sans', 
+        'sans-serif', 
+        'Apple Color Emoji', 
+        'Segoe UI Emoji', 
+        'Segoe UI Symbol', 
+        'Noto Color Emoji'
+      ],
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
-  },
+
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-  ]
+  ],
 }
