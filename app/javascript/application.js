49ff486e6740fdffb1a3bfc5c01b6f2e53b2cd7d
Ben Maurer <benzooku@gmail.com>
2024-06-21 19:09:34 +0200

    importmap setup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,1 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
-import React from "react"
-import ReactDOM from "react-dom"
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,3 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "react", preload: true # @18.2.0
-pin "react-dom", preload: true # @18.2.0
-pin "scheduler", preload: true # @0.23.0
+pin "application"
